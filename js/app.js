36f9c06ead3fa9ecafb416d7f8bf804ee6ea745f
Armando Rivera <rivera.armando997@gmail.com>
2022-01-21 20:27:22 -0600

    Filter, image cover, personalized page finished

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
 const buildForYou = document.getElementById("build-for-you");
+const imageCover = document.getElementById("image-cover");
+const layoutMain = document.getElementById("layout-main");
 const buildForYouSection = document.getElementById("buildForYouSection");
 
 const home = document.getElementById("home");
@@ -8,12 +10,13 @@ const landing = document.getElementById("landing");
 buildForYou.addEventListener("click", () => {
   landing.style.display = "none";
   buildForYouSection.style.display = "block";
+  imageCover.style.display = "block";
+  layoutMain.classList.add("is-showBackImage");
 });
 
 home.addEventListener("click", () => {
   landing.style.display = "block";
   buildForYouSection.style.display = "none";
+  imageCover.style.display = "none";
+  layoutMain.classList.remove("is-showBackImage");
 });
-
-landing.style.display = "none";
-buildForYouSection.style.display = "block";
