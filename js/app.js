2fadc8f7f85db47b18c9a7dd538fa468341aba43
Armando Rivera <rivera.armando997@gmail.com>
2022-01-21 22:47:41 -0600

    Recently listened page created

--- js/app.js
+++ js/app.js
@@ -2,21 +2,34 @@ const buildForYou = document.getElementById("build-for-you");
 const imageCover = document.getElementById("image-cover");
 const layoutMain = document.getElementById("layout-main");
 const buildForYouSection = document.getElementById("buildForYouSection");
+const recentlyListened = document.getElementById("recently-listened");
+const recentlyListenedSection = document.getElementById("recentlyListened");
 
 const home = document.getElementById("home");
 
 const landing = document.getElementById("landing");
 
+recentlyListened.addEventListener("click", () => {
+  resetLayout();
+  recentlyListenedSection.style.display = "block";
+});
+
 buildForYou.addEventListener("click", () => {
-  landing.style.display = "none";
+  resetLayout();
   buildForYouSection.style.display = "block";
   imageCover.style.display = "block";
   layoutMain.classList.add("is-showBackImage");
 });
 
 home.addEventListener("click", () => {
+  resetLayout();
   landing.style.display = "block";
-  buildForYouSection.style.display = "none";
-  imageCover.style.display = "none";
   layoutMain.classList.remove("is-showBackImage");
 });
+
+function resetLayout() {
+  landing.style.display = "none";
+  buildForYouSection.style.display = "none";
+  imageCover.style.display = "none";
+  recentlyListenedSection.style.display = "none";
+}
